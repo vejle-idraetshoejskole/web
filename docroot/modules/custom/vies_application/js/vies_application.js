a34a8f92ae24a80d3f699a5e8dabc64ae423d4de
Andriy Iun <andriy.yun@gmail.com>
2018-07-02 18:16:30 +0200

    VI-455 Improved question adding process

---
+++ docroot/modules/custom/vies_application/js/vies_application.js
@@ -0,0 +1,20 @@
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.questionInlineEntityForm = {
+    attach: function (context, drupalSettings) {
+      $('.questions-inline-form-type').once().change(function() {
+        var _this = $(this);
+        var $answer_container = _this.parents('.questions-inline-form').find('.field--name-field-vies-answer');
+        console.log(_this.val());
+        console.log($answer_container);
+        if (_this.val() == 'textfield') {
+          $answer_container.hide();
+        }
+        else {
+          $answer_container.show();
+        }
+      }).change();
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
